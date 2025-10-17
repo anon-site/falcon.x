123f753110d1ea90a5bc55bc3df42e24a584fc53
ANON <112594378+anon-site@users.noreply.github.com>
2025-10-17 11:27:34 +0300

    Update data.js from Admin Panel

--- js/data.js
+++ js/data.js
@@ -112,10 +112,14 @@ const androidApps = [
         version: '1.4.0',
         category: 'general',
         icon: 'https://play-lh.googleusercontent.com/StTPXVol1bhGB96HxBqfoXy7B40r4bl1-4J-Z6LiJ18eGA8JYb7GXCRKs2ywwewVhnbLiDQERYF0SfstvGnn=w600-h300-pc0xffffff-pd',
-        description: 'SSVid: The All-in-One Video Downloading Solution for Free and Unlimited Downloads',
-        size: '19.21',
-        downloadLink: 'https://www.mediafire.com/file/rhl3d8hxlce1i4e/SSVID_1.4.0.apks/file',
-        isModified: false
+        description: 'SSVid is a free Android application that allows users to search, stream, and download videos and audio from various platforms such as YouTube and Facebook.',
+        fullDescription: 'SSVid is an all-in-one media downloader for Android devices. It enables users to search for content, stream it instantly, or download it for offline enjoyment. The app supports multiple quality options for streaming and downloading, offers background playback support, and allows users to create and manage playlists. It provides a clean, ad-free experience, making it an excellent choice for media enthusiasts.',
+        size: '28.17 MB',
+        downloadLink: '#',
+        originalDownloadLink: 'https://www.mediafire.com/file/rhl3d8hxlce1i4e/SSVID_1.4.0.apks/file',
+        isModified: false,
+        screenshots: ['https://image.winudf.com/v2/image1/Y29tLmFsbHZpZGVvZG93bmxvYWRlci51Y2Rvd25sb2FkZXJfc2NyZWVuXzBfMTY2MTU4MzQ3Ml8wOTY/screen-0.jpg?fakeurl=1&type=.jpg', 'https://play-lh.googleusercontent.com/6AgmhIONW2oII1hhgiQGZlL0-OtarEFU7-qn6_i8wX9GyEn9RiSXZVjzUVFRbdLZvA=w526-h296-rw', 'https://www.techuseful.com/wp-content/uploads/2020/12/Allvideodownloader.jpg'],
+        features: ['Fast and reliable media search', 'Stream videos in multiple quality options', 'Download videos and audio in various formats', 'Background playback support', 'Create and manage playlists', 'Clean, ad-free experience']
     },
     {
         id: 1760627555294,
